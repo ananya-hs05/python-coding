882e55eee5937ccf347bbb348dcea2eeba1aeef1
ananya-hs05 <hsananya524@gmail.com>
2025-07-18 20:19:31 +0530

    program1.py

--- program1.py
+++ program1.py
@@ -1,1 +1,24 @@
+class Calculator:
+    def __init__(self, a, b, operation):
+        self.a = float(a)
+        self.b = float(b)
+        self.operation = operation
+
+    def calculate(self):
+        if self.operation == "add":
+            return self.a + self.b
+        elif self.operation == "subtract":
+            return self.a - self.b
+        elif self.operation == "multiply":
+            return self.a * self.b
+        elif self.operation == "divide":
+            return self.a / self.b  # no zero division handling
+        else:
+            return "Invalid operation"
+
+a = input("Enter value of a: ")
+b = input("Enter value of b: ")
+op = input("Enter operation: ")
+calc = Calculator(a, b, op)
+print("Result is:", calc.calculate())
 
